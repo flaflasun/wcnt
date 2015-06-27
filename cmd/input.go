dc0eab1fb3c26dd462a25ccfe77f15843b7ffbfc
flaflasun <k.hata.work+github@gmail.com>
2015-06-27 19:45:40 +0900

    Add Windows support
    In the case of Windows os.stdin.stat returns an error when STDIN is a tty or NUL. Modified so that it does not confirm the os.stdin.stat on Windows.

--- cmd/input.go
+++ cmd/input.go
@@ -3,22 +3,56 @@ package cmd
 import (
 	"io/ioutil"
 	"os"
+	"runtime"
 )
 
 func getInput(args string) (string, error) {
-	stdin := os.Stdin
-	stat, err := stdin.Stat()
+	input, err := getPipe()
 	if err != nil {
 		return "", err
 	}
-	if (stat.Mode() & os.ModeCharDevice) == 0 {
-		bytes, err := ioutil.ReadAll(stdin)
+	if len(input) == 0 {
+		input, err = getArgs(args)
 		if err != nil {
 			return "", err
 		}
-		return string(bytes), nil
 	}
+	return input, nil
+}
 
+func Exists(name string) bool {
+	_, err := os.Stat(name)
+	return err == nil
+}
+
+func getPipe() (string, error) {
+	stdin := os.Stdin
+	stat, err := stdin.Stat()
+	if runtime.GOOS == "windows" {
+		if err == nil {
+			return getStdin(stdin)
+		}
+	} else {
+		if err != nil {
+			return "", err
+		}
+		mode := stat.Mode()
+		if (mode & os.ModeNamedPipe != 0) || mode.IsRegular() {
+			return getStdin(stdin)
+		}
+	}
+	return "" ,nil
+}
+
+func getStdin(stdin *os.File) (string, error) {
+	bytes, err := ioutil.ReadAll(stdin)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func getArgs(args string) (string, error) {
 	if Exists(args) {
 		content, err := ioutil.ReadFile(args)
 		if err != nil {
@@ -27,9 +61,4 @@ func getInput(args string) (string, error) {
 		return string(content), nil
 	}
 	return args, nil
-}
-
-func Exists(name string) bool {
-	_, err := os.Stat(name)
-	return err == nil
 }
