184c49eed868e38b02bcab0cacc0daec94bf9992
flaflasun <k.hata.work+github@gmail.com>
2015-07-12 14:10:40 +0900

    Clean options processing

--- cmd/opt.go
+++ cmd/opt.go
@@ -1,27 +1,37 @@
 package cmd
 
-import (
-	"strings"
-)
+import "strings"
 
-func ignoreChar(t, ign string) string {
+type editor struct {
+	text string
+}
+
+func newEditor(input string) *editor {
+	e := new(editor)
+	e.text = input
+	return e
+}
+
+func (e *editor) ignoreChar(ign string) {
 	if len(ign) > 0 {
 		igns := make([]string, 1, len(ign))
 		igns = strings.Split(ign, "")
 		for _, c := range igns {
-			t = strings.Replace(t, c, "", -1)
+			e.text = strings.Replace(e.text, c, "", -1)
 		}
 	}
-	return t
 }
 
-func addDelimiter(t, del string) string {
+func (e *editor) addDelimiter(del string) {
 	if len(del) > 0 {
 		delimiters := make([]string, 1, len(del))
 		delimiters = strings.Split(del, "")
 		for _, c := range delimiters {
-			t = strings.Replace(t, c, " ", -1)
+			e.text = strings.Replace(e.text, c, " ", -1)
 		}
 	}
-	return t
+}
+
+func (e *editor) ignorecase() {
+	e.text = strings.ToLower(e.text)
 }
--- cmd/wordc.go
+++ cmd/wordc.go
@@ -3,26 +3,25 @@ package cmd
 import (
 	"log"
 	"sort"
-	"strings"
 
 	"github.com/codegangsta/cli"
 )
 
 func Wordc(c *cli.Context) {
-	words := make(Words, 0, 0)
-	ign := c.String("ignorechar")
-	del := c.String("delimiter")
-	text, err := getInput(c.Args().Get(0))
+	input, err := getInput(c.Args().Get(0))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	text = ignoreChar(text, ign)
-	text = addDelimiter(text, del)
+	edit := newEditor(input)
+	edit.ignoreChar(c.String("ignorechar"))
+	edit.addDelimiter(c.String("delimiter"))
 	if c.Bool("ignorecase") {
-		text = strings.ToLower(text)
+		edit.ignorecase()
 	}
-	words.AddWords(text)
+
+	words := make(Words, 0, 0)
+	words.AddWords(edit.text)
 
 	if c.Bool("reverse") {
 		sort.Sort(words)
